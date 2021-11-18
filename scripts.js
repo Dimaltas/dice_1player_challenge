1cf7ad34b17fd612a040424ce56dd9ba3b789d6a
Dimaltas <d.maltas@hotmail.com>
2021-11-18 11:01:01 +0000

    draft

---
+++ scripts.js
@@ -0,0 +1,31 @@
+var sum =0
+function rollTheDice() {
+    const num1 = Math.floor(Math.random()*6)+1
+    const diceImage = "pictures/dice" + num1 +".png";
+    document.querySelectorAll('img')[0].setAttribute('src',diceImage);
+    console.log(num1);
+    sum = sum + num1;
+    console.log(sum);
+    document.querySelector('p').innerHTML = "The sum is "+sum;
+    if(sum >20){
+        document.querySelector('p').innerHTML = "Congratulations you won!";
+        sum = 0;
+    }else if(num1 == 1){
+        document.querySelector('p').innerHTML = "You lose";
+        sum = 0;
+    }
+}
+
+
+
+
+
+
+
+// setTimeout(function () {                        //trying to make the animation (shake and then show)!!!!
+//     const num1 = Math.floor(Math.random()*6)+1
+//     const diceImage = "pictures/dice" + num1 +".png";
+//     document.querySelectorAll('img')[0].setAttribute('src',diceImage);
+// }, 0);
+
+        
